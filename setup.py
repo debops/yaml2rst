358dbed575867339ff0393266c341a9fae0399c7
Hartmut Goebel <h.goebel@crazy-compilers.com>
2015-02-09 14:38:44 +0100

    Update Development Status to "Beta".

--- setup.py
+++ setup.py
@@ -22,8 +22,7 @@ setup(
     #download_url='http://yml2rst-%s.tar.gz' % __version__,
     keywords=['YML', 'rst', 'reStructuresText'],
     classifiers=[
-        #'Development Status :: 4 - Beta',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
